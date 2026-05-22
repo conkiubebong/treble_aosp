3870df64d61a015906615b3c3b8cebfeacfb1d7c
conkiubebong <minhle2002dhcn@gmail.com>
2026-05-22 14:37:30 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ initRepos() {
 
 syncRepos() {
     echo "--> Syncing repos"
-    repo sync -c --force-sync --no-clone-bundle --no-tags -j$8 || repo sync -c --force-sync --no-clone-bundle --no-tags -j$8
+    repo sync -c --force-sync --no-clone-bundle --no-tags -j8 || repo sync -c --force-sync --no-clone-bundle --no-tags -j8
     echo
 }
 
