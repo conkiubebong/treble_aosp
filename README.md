c9128d2552b54e3f5969b051d5519d0d28ce7bee
Đào Mai Auto <daomai.auto@gmail.com>
2026-06-02 18:48:24 +0700

    android 12

--- README.md
+++ README.md
@@ -1,31 +1,57 @@
-# TrebleDroid AOSP GSI
+# Community
 
-## Build
-To get started with building AOSP GSI, you'll need to get familiar with [Git and Repo](https://source.android.com/source/using-repo.html) as well as [How to build a GSI](https://github.com/phhusson/treble_experimentations/wiki/How-to-build-a-GSI%3F).
-- Create a new working directory for your AOSP build and navigate to it:
-    ```
-    mkdir aosp; cd aosp
-    ```
-- Clone this repo:
-    ```
-    git clone https://github.com/conkiubebong/treble_aosp -b android-14.0
-    ```
-- Finally, start the build script:
-    ```
-    bash treble_aosp/build.sh
-    ```
+* IRC: irc://irc.freenode.net/#phh-treble
+* WebIRC: http://webchat.freenode.net/?channels=%23phh-treble&uio=d4
+* Matrix: [#phh-treble:matrix.org](https://matrix.to/#/#phh-treble:matrix.org)
+* Telegram https://t.me/phhtreble
+* xda-developers threads: https://forum.xda-developers.com/search.php?do=finduser&u=1915408&starteronly=1
 
-## Issues
-[Open issue](https://github.com/ponces/treble_aosp/issues/new/choose)
+# How to build
 
-## Credits
-These people have helped this project in some way or another, so they should be the ones who receive all the credit:
-- [phhusson](https://github.com/phhusson)
-- [AndyYan](https://github.com/AndyCGYan)
-- [eremitein](https://github.com/eremitein)
-- [kdrag0n](https://github.com/kdrag0n)
-- [Peter Cai](https://github.com/PeterCxy)
-- [haridhayal11](https://github.com/haridhayal11)
-- [sooti](https://github.com/sooti)
-- [Iceows](https://github.com/Iceows)
-- [ChonDoit](https://github.com/ChonDoit)
+* clone this repository
+* call the build scripts from a separate directory
+
+For example:
+
+    git clone https://github.com/phhusson/treble_experimentations
+    mkdir Lineage; cd Lineage
+    bash ../treble_experimentations/build-rom.sh android-8.1 lineage
+
+## More flexible build script
+
+(this has been tested much less)
+
+  bash ../treble_experimentations/build-dakkar.sh rr \
+    arm-aonly-gapps-su \
+    arm64-ab-go-nosu
+
+The script should provide a help message if you pass something it
+doesn't understand
+
+# Using Docker
+
+clone this repository, then:
+
+    docker build -t treble docker/
+    
+    docker container create --name treble treble
+    
+    docker run -ti \
+        -v $(pwd):/treble \
+        -v $(pwd)/../treble_output:/treble_output \
+        -w /treble_output \
+        treble \
+        /bin/bash /treble/build-dakkar.sh rr \
+        arm-aonly-gapps-su \
+        arm64-ab-go-nosu
+
+# Conventions for commit messages:
+
+* `[UGLY]` Please make this patch disappear as soon as possible
+* `[master]` tag means that the commit should be dropped in a future
+  rebase
+* `[device]` tag means this change is device-specific workaround
+* `::device name::` will try to describe which devices are concerned
+  by this change
+* `[userfriendly]` This commit is NOT used for hardware support, but
+  to make the rom more user friendly
--- build.sh
+++ build.sh
@@ -1,156 +1,231 @@
 #!/bin/bash
 
-echo
-echo "--------------------------------------"
-echo "          AOSP 14.0 Buildbot          "
-echo "                  by                  "
-echo "                ponces                "
-echo "--------------------------------------"
-echo
-
+rom_fp="$(date +%y%m%d)"
+originFolder="$(dirname "$(readlink -f -- "$0")")"
+mkdir -p release/$rom_fp/
 set -e
 
-BL=$PWD/treble_aosp
-BD=$HOME/builds
-BV=$1
+if [ -z "$USER" ];then
+	export USER="$(id -un)"
+fi
+export LC_ALL=C
 
-initRepos() {
-    echo "--> Initializing workspace"
-    repo init -u https://android.googlesource.com/platform/manifest -b android-14.0.0_r55 --git-lfs
-    echo
+manifest_url="https://android.googlesource.com/platform/manifest"
+aosp="android-8.1.0_r65"
+phh="android-8.1"
 
-    echo "--> Preparing local manifest"
-    mkdir -p .repo/local_manifests
-    cp $BL/build/default.xml .repo/local_manifests/default.xml
-    cp $BL/build/remove.xml .repo/local_manifests/remove.xml
-    echo
-}
+build_target="$1"
+rebuild_release=""
+if [ "$1" == "android-9.0" ];then
+    manifest_url="https://gitlab.com/aosp-security/manifest"
+    aosp="android-9.0.0_r53-r47"
+    phh="android-9.0"
+elif [ "$1" == "android-10.0" ];then
+    manifest_url="https://android.googlesource.com/platform/manifest"
+    aosp="android-10.0.0_r41"
+    phh="android-10.0"
+elif [ "$1" == "android-11.0" ];then
+    manifest_url="https://android.googlesource.com/platform/manifest"
+    aosp="android-11.0.0_r48"
+    phh="android-11.0"
+elif [ "$1" == "android-12.0" ];then
+    manifest_url="https://android.googlesource.com/platform/manifest"
+    aosp="android-12.1.0_r11"
+    phh="android-12.0"
+else
+	# guess android version from version number
+	rebuild_release="yes"
+	if [ -n "$(echo $1 | grep -E '^v4..')" ];then
+		build_target="android-12.0"
+	elif [ -n "$(echo $1 | grep -E '^v3..')" ];then
+		build_target="android-11.0"
+	elif [ -n "$(echo $1 | grep -E '^v2..')" ];then
+		build_target="android-10.0"
+	elif [ -n "$(echo $1 | grep -E '^v1..')" ];then
+		build_target="android-9.0"
+	else
+		build_target="android-8.1"
+	fi
+	# download manifest with the given version number
+	tmp_manifest_source=$(mktemp -d)
+	wget "https://github.com/phhusson/treble_experimentations/releases/download/$1/manifest.xml" -O $tmp_manifest_source/manifest.xml
+	sed -i 's/<remote name="aosp" fetch=".." review="https:\/\/android-review.googlesource.com\/"\/>/<remote name="aosp" fetch="https:\/\/android.googlesource.com\/" review="https:\/\/android-review.googlesource.com\/"\/>/' $tmp_manifest_source/manifest.xml
+	(cd $tmp_manifest_source; git init; git add manifest.xml; git commit -m "$1")
+fi
 
-syncRepos() {
-    echo "--> Syncing repos"
-    repo sync -c --force-sync --no-clone-bundle --no-tags -j8 || repo sync -c --force-sync --no-clone-bundle --no-tags -j8
-    echo
-}
+if [ "$release" == true ];then
+    [ -z "$version" ] && exit 1
+    [ ! -f "$originFolder/release/config.ini" ] && exit 1
+fi
 
-applyPatches() {
-    echo "--> Applying TrebleDroid patches"
-    bash $BL/patch.sh $BL trebledroid
-    echo
+if [ -n "$rebuild_release" ];then
+	repo init -u "$tmp_manifest_source" -m manifest.xml --depth=1
+else
+	repo init -u "$manifest_url" -b $aosp --depth=1
+	if [ -d .repo/local_manifests ] ;then
+		( cd .repo/local_manifests; git fetch; git reset --hard; git checkout origin/$phh)
+	else
+		git clone https://github.com/phhusson/treble_manifest .repo/local_manifests -b $phh
+	fi
+fi
+repo sync -c -j 1 --force-sync || repo sync -c -j1 --force-sync
 
-    echo "--> Applying personal patches"
-    bash $BL/patch.sh $BL personal
-    echo
+repo forall -r '.*opengapps.*' -c 'git lfs fetch && git lfs checkout'
+(cd device/phh/treble; git clean -fdx; if [ -f phh.mk ];then bash generate.sh phh;else bash generate.sh;fi)
+(cd vendor/foss; git clean -fdx; bash update.sh)
+if [ "$build_target" == "android-12.0" ] && grep -q lottie packages/apps/Launcher3/Android.bp;then
+    (cd vendor/partner_gms; git am $originFolder/0001-Fix-SearchLauncher-for-Android-12.1.patch || true)
+    (cd vendor/partner_gms; git am $originFolder/0001-Update-SetupWizard-to-A12.1-to-fix-fingerprint-enrol.patch || true)
+fi
+rm -f vendor/gapps/interfaces/wifi_ext/Android.bp
 
-    echo "--> Generating makefiles"
-    cd device/phh/treble
-    cp $BL/build/aosp.mk .
-    bash generate.sh aosp
-    cd ../../..
-    echo
-}
-
-setupEnv() {
-    echo "--> Setting up build environment"
-    source build/envsetup.sh &>/dev/null
-    mkdir -p $BD
-    echo
-}
-
-buildTrebleApp() {
-    echo "--> Building treble_app"
-    cd treble_app
-    bash build.sh release
-    cp TrebleApp.apk ../vendor/hardware_overlay/TrebleApp/app.apk
-    cd ..
-    echo
-}
+. build/envsetup.sh
 
 buildVariant() {
-    echo "--> Building $1"
-    lunch "$1"-ap2a-userdebug
-    make -j$(nproc --all) installclean
-    make -j$(nproc --all) systemimage
-    make -j$(nproc --all) target-files-package otatools
-    bash $BL/sign.sh "vendor/ponces-priv/keys" $OUT/signed-target_files.zip
-    unzip -jo $OUT/signed-target_files.zip IMAGES/system.img -d $OUT
-    mv $OUT/system.img $BD/system-"$1".img
-    echo
+	lunch $1
+	make RELAX_USES_LIBRARY_CHECK=true BUILD_NUMBER=$rom_fp installclean
+	make RELAX_USES_LIBRARY_CHECK=true BUILD_NUMBER=$rom_fp -j8 systemimage
+	make RELAX_USES_LIBRARY_CHECK=true BUILD_NUMBER=$rom_fp vndk-test-sepolicy
+	xz -c $OUT/system.img -T0 > release/$rom_fp/system-${2}.img.xz
 }
 
-buildVndkliteVariant() {
-    echo "--> Building $1-vndklite"
-    [[ "$1" == *"a64"* ]] && arch="32" || arch="64"
-    cd treble_adapter
-    sudo bash lite-adapter.sh "$arch" $BD/system-"$1".img
-    mv s.img $BD/system-"$1"-vndklite.img
-    sudo rm -rf d tmp
-    cd ..
-    echo
-}
+repo manifest -r > release/$rom_fp/manifest.xml
+bash "$originFolder"/list-patches.sh
+cp patches.zip release/$rom_fp/patches-for-developers.zip
 
-buildVariants() {
-    # buildVariant treble_a64_bvN
-    # buildVariant treble_a64_bgN
-    # buildVariant treble_arm64_bvN
-    buildVariant treble_arm64_bgN
-    # buildVndkliteVariant treble_a64_bvN
-    # buildVndkliteVariant treble_a64_bgN
-    # buildVndkliteVariant treble_arm64_bvN
-    # buildVndkliteVariant treble_arm64_bgN
-}
+if [ "$build_target" == "android-12.0" ];then
+    (
+        git clone https://github.com/phhusson/sas-creator
+        cd sas-creator
 
-generatePackages() {
-    echo "--> Generating packages"
-    buildDate="$(date +%Y%m%d)"
-    find $BD/ -name "system-treble_*.img" | while read file; do
-        filename="$(basename $file)"
-        [[ "$filename" == *"_a64"* ]] && arch="arm32_binder64" || arch="arm64"
-        [[ "$filename" == *"_bvN"* ]] && variant="vanilla" || variant="gapps"
-        [[ "$filename" == *"-vndklite"* ]] && vndk="-vndklite" || vndk=""
-        name="aosp-${arch}-ab-${variant}${vndk}-14.0-$buildDate"
-        xz -cv "$file" -T0 > $BD/"$name".img.xz
-    done
-    rm -rf $BD/system-*.img
-    echo
-}
+        git clone https://github.com/phhusson/vendor_vndk -b android-10.0
+    )
 
-generateOta() {
-    echo "--> Generating OTA file"
-    version="$(date +v%Y.%m.%d)"
-    buildDate="$(date +%Y%m%d)"
-    timestamp="$START"
-    json="{\"version\": \"$version\",\"date\": \"$timestamp\",\"variants\": ["
-    find $BD/ -name "aosp-*-14.0-$buildDate.img.xz" | sort | {
-        while read file; do
-            filename="$(basename $file)"
-            [[ "$filename" == *"-arm32"* ]] && arch="a64" || arch="arm64"
-            [[ "$filename" == *"-vanilla"* ]] && variant="v" || variant="g"
-            [[ "$filename" == *"-vndklite"* ]] && vndk="-vndklite" || vndk=""
-            name="treble_${arch}_b${variant}N${vndk}"
-            size=$(wc -c $file | awk '{print $1}')
-            url="https://github.com/ponces/treble_aosp/releases/download/$version/$filename"
-            json="${json} {\"name\": \"$name\",\"size\": \"$size\",\"url\": \"$url\"},"
-        done
-        json="${json%?}]}"
-        echo "$json" | jq . > $BL/config/ota.json
-    }
-    echo
-}
+	buildVariant treble_arm64_bvS-userdebug squeak-arm64-ab-vanilla
+    ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-squeak-arm64-ab-vndklite-vanilla.img.xz )
+    ( cd sas-creator; bash securize.sh s.img; xz -c s-secure.img -T0 > ../release/$rom_fp/system-squeak-arm64-ab-vndklite-vanilla-secure.img.xz )
 
-START=$(date +%s)
+	buildVariant treble_arm64_bgS-userdebug squeak-arm64-ab-gapps
+    ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-squeak-arm64-ab-vndklite-gapps.img.xz )
+    ( cd sas-creator; bash securize.sh s.img; xz -c s-secure.img -T0 > ../release/$rom_fp/system-squeak-arm64-ab-vndklite-gapps-secure.img.xz )
 
-initRepos
-syncRepos
-applyPatches
-setupEnv
-buildTrebleApp
-[ ! -z "$BV" ] && buildVariant "$BV" || buildVariants
-generatePackages
-generateOta
+	buildVariant treble_arm64_boS-userdebug squeak-arm64-ab-gogapps
+    ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-squeak-arm64-ab-vndklite-gogapps.img.xz )
+    ( cd sas-creator; bash securize.sh s.img; xz -c s-secure.img -T0 > ../release/$rom_fp/system-squeak-arm64-ab-vndklite-gogapps-secure.img.xz )
 
-END=$(date +%s)
-ELAPSEDM=$(($(($END-$START))/60))
-ELAPSEDS=$(($(($END-$START))-$ELAPSEDM*60))
+	buildVariant treble_arm64_bfS-userdebug squeak-arm64-ab-floss
+    ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-squeak-arm64-ab-vndklite-floss.img.xz )
+    ( cd sas-creator; bash securize.sh s.img; xz -c s-secure.img -T0 > ../release/$rom_fp/system-squeak-arm64-ab-vndklite-floss-secure.img.xz )
 
-echo "--> Buildbot completed in $ELAPSEDM minutes and $ELAPSEDS seconds"
-echo
+	buildVariant treble_a64_bvS-userdebug squeak-arm32_binder64-ab-vanilla
+    ( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-squeak-arm32_binder64-ab-vndklite-vanilla.img.xz )
+    ( cd sas-creator; bash securize.sh s.img; xz -c s-secure.img -T0 > ../release/$rom_fp/system-squeak-arm32_binder64-ab-vndklite-vanilla-secure.img.xz )
+
+	buildVariant treble_a64_boS-userdebug squeak-arm32_binder64-ab-gogapps
+    ( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-squeak-arm32_binder64-ab-vndklite-gogapps.img.xz )
+    ( cd sas-creator; bash securize.sh s.img; xz -c s-secure.img -T0 > ../release/$rom_fp/system-squeak-arm32_binder64-ab-vndklite-gogapps-secure.img.xz )
+elif [ "$build_target" == "android-11.0" ];then
+    (
+        git clone https://github.com/phhusson/sas-creator
+        cd sas-creator
+
+        git clone https://github.com/phhusson/vendor_vndk -b android-10.0
+    )
+
+    # ARM64 vanilla {ab, a-only, ab vndk lite}
+	buildVariant treble_arm64_bvS-userdebug roar-arm64-ab-vanilla
+    ( cd sas-creator; bash run.sh 64 ; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-aonly-vanilla.img.xz)
+    ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-ab-vndklite-vanilla.img.xz )
+
+    # ARM64 floss {ab, a-only, ab vndk lite}
+	buildVariant treble_arm64_bfS-userdebug roar-arm64-ab-floss
+    ( cd sas-creator; bash run.sh 64 ; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-aonly-floss.img.xz)
+    ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-ab-vndklite-floss.img.xz )
+
+    # ARM32 vanilla {ab, a-only}
+	buildVariant treble_arm_bvS-userdebug roar-arm-ab-vanilla
+    ( cd sas-creator; bash run.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm-aonly-vanilla.img.xz )
+
+    # ARM32 gogapps {ab, a-only}
+	buildVariant treble_arm_boS-userdebug roar-arm-ab-gogapps
+    ( cd sas-creator; bash run.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm-aonly-gogapps.img.xz )
+
+    # ARM32_binder64 vanilla {ab, ab vndk lite}
+	buildVariant treble_a64_bvS-userdebug roar-arm32_binder64-ab-vanilla
+    ( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm32_binder64-ab-vndklite-vanilla.img.xz)
+
+    # ARM64 Gapps {ab, a-only, ab vndk lite}
+	buildVariant treble_arm64_bgS-userdebug roar-arm64-ab-gapps
+    ( cd sas-creator; bash run.sh 64 ; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-aonly-gapps.img.xz)
+    ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-ab-vndklite-gapps.img.xz )
+
+    # ARM32_binder64 go gapps {ab, ab vndk lite}
+	buildVariant treble_a64_boS-userdebug roar-arm32_binder64-ab-gogapps
+    ( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm32_binder64-ab-vndklite-gogapps.img.xz )
+
+    # ARM32_binder64 gapps {ab, ab vndk lite}
+	buildVariant treble_a64_bgS-userdebug roar-arm32_binder64-ab-gapps
+    ( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm32_binder64-ab-vndklite-gapps.img.xz )
+elif [ "$build_target" == "android-10.0" ];then
+	buildVariant treble_arm64_afS-userdebug quack-arm64-aonly-floss
+	buildVariant treble_arm64_avS-userdebug quack-arm64-aonly-vanilla
+	buildVariant treble_arm64_agS-userdebug quack-arm64-aonly-gapps
+	buildVariant treble_arm64_aoS-userdebug quack-arm64-aonly-go
+	buildVariant treble_arm64_bfS-userdebug quack-arm64-ab-floss
+	buildVariant treble_arm64_bvS-userdebug quack-arm64-ab-vanilla
+	buildVariant treble_arm64_bgS-userdebug quack-arm64-ab-gapps
+	buildVariant treble_arm64_boS-userdebug quack-arm64-ab-go
+	buildVariant treble_arm_avS-userdebug quack-arm-aonly-vanilla
+	buildVariant treble_arm_agS-userdebug quack-arm-aonly-gapps
+	buildVariant treble_arm_aoS-userdebug quack-arm-aonly-go
+	buildVariant treble_arm_bvS-userdebug quack-arm-ab-vanilla
+	buildVariant treble_arm_bgS-userdebug quack-arm-ab-gapps
+	buildVariant treble_arm_boS-userdebug quack-arm-ab-go
+	buildVariant treble_a64_avS-userdebug quack-arm32_binder64-aonly-vanilla
+	buildVariant treble_a64_agS-userdebug quack-arm32_binder64-aonly-gapps
+	buildVariant treble_a64_aoS-userdebug quack-arm32_binder64-aonly-go
+	buildVariant treble_a64_bvS-userdebug quack-arm32_binder64-ab-vanilla
+	buildVariant treble_a64_bgS-userdebug quack-arm32_binder64-ab-gapps
+	buildVariant treble_a64_boS-userdebug quack-arm32_binder64-ab-go
+else
+	buildVariant treble_arm64_avN-userdebug arm64-aonly-vanilla-nosu
+	buildVariant treble_arm64_agS-userdebug arm64-aonly-gapps-su
+	#buildVariant treble_arm64_afS-userdebug arm64-aonly-floss-su
+	rm -Rf out/target/product/phhgsi*
+
+	buildVariant treble_arm64_bvN-userdebug arm64-ab-vanilla-nosu
+	buildVariant treble_arm64_bgS-userdebug arm64-ab-gapps-su
+	#buildVariant treble_arm64_bfS-userdebug arm64-ab-floss-su
+	rm -Rf out/target/product/phhgsi*
+
+	buildVariant treble_arm_avN-userdebug arm-aonly-vanilla-nosu
+	[ "$build_target" != "android-9.0" ] && buildVariant treble_arm_aoS-userdebug arm-aonly-go-su
+	buildVariant treble_arm_agS-userdebug arm-aonly-gapps-su
+	rm -Rf out/target/product/phhgsi*
+
+	buildVariant treble_a64_avN-userdebug arm32_binder64-aonly-vanilla-nosu
+	buildVariant treble_a64_agS-userdebug arm32_binder64-aonly-gapps-su
+	rm -Rf out/target/product/phhgsi*
+
+	if [ "$build_target" == "android-9.0" ];then
+	buildVariant treble_a64_bvN-userdebug arm32_binder64-ab-vanilla-nosu
+	buildVariant treble_a64_bgS-userdebug arm32_binder64-ab-gapps-su
+	fi
+	rm -Rf out/target/product/phhgsi*
+fi
+
+if [ "$release" == true ];then
+    (
+        rm -Rf venv
+        pip install virtualenv
+        export PATH=$PATH:~/.local/bin/
+        virtualenv -p /usr/bin/python3 venv
+        source venv/bin/activate
+        pip install -r $originFolder/release/requirements.txt
+
+        name="AOSP 8.1"
+        [ "$build_target" == "android-9.0" ] && name="AOSP 9.0"
+        python $originFolder/release/push.py "$name" "$version" release/$rom_fp/
+        rm -Rf venv
+    )
+fi
